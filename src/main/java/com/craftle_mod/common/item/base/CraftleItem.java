96632d2f59069ddf6ba3f664f9292c95f5c36ba7
Kalindu De Costa <kalinduk.decosta@gmail.com>
2020-06-03 19:23:04 -0400

    adding resource items

--- src/main/java/com/craftle_mod/common/item/base/CraftleItem.java
+++ src/main/java/com/craftle_mod/common/item/base/CraftleItem.java
@@ -4,21 +4,34 @@ import com.craftle_mod.common.CraftleCreativeTabs;
 import com.craftle_mod.common.resource.Resource;
 import com.craftle_mod.common.resource.ResourceType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class CraftleItem extends Item {
 
     private final Resource     resource;
     private final ResourceType type;
+    private final boolean      hasEffect;
 
     public CraftleItem(Resource resource, ResourceType type) {
-        super(getProperties(type));
-        this.resource = resource;
-        this.type     = type;
+        this(resource, type, false);
 
+    }
+
+    public CraftleItem(Resource resource, ResourceType type,
+                       boolean hasEffect) {
+        super(getProperties(type));
+        this.resource  = resource;
+        this.type      = type;
+        this.hasEffect = hasEffect;
     }
 
     public String getCraftleRegistryName() {
         return resource.getResourceName() + "_" + type.getResourceName();
+    }
+
+    @Override
+    public boolean hasEffect(ItemStack stack) {
+        return hasEffect;
     }
 
     public Resource getResource() {
--- src/main/java/com/craftle_mod/common/registries/CraftleItems.java
+++ src/main/java/com/craftle_mod/common/registries/CraftleItems.java
@@ -78,18 +78,84 @@ public class CraftleItems {
             registerResource(Resource.TIN, ResourceType.BIT);
     public static final RegistryObject<Item> URANIUM_BIT   =
             registerResource(Resource.URANIUM, ResourceType.BIT);
+
     //dusts
+    public static final RegistryObject<Item> ALUMINIUM_DUST =
+            registerResource(Resource.ALUMINIUM, ResourceType.DUST);
+    public static final RegistryObject<Item> COPPER_DUST    =
+            registerResource(Resource.COPPER, ResourceType.DUST);
+    public static final RegistryObject<Item> DIAMOND_DUST   =
+            registerResource(Resource.DIAMOND, ResourceType.DUST);
+    public static final RegistryObject<Item> GOLD_DUST      =
+            registerResource(Resource.GOLD, ResourceType.DUST);
+    public static final RegistryObject<Item> IRON_DUST      =
+            registerResource(Resource.IRON, ResourceType.DUST);
+    public static final RegistryObject<Item> PLATINUM_DUST  =
+            registerResource(Resource.PLATINUM, ResourceType.DUST);
+    public static final RegistryObject<Item> RUBY_DUST      =
+            registerResource(Resource.RUBY, ResourceType.DUST);
+    public static final RegistryObject<Item> SAPPHIRE_DUST  =
+            registerResource(Resource.SAPPHIRE, ResourceType.DUST);
+    public static final RegistryObject<Item> STEEL_DUST     =
+            registerResource(Resource.STEEL, ResourceType.DUST);
+    public static final RegistryObject<Item> TIN_DUST       =
+            registerResource(Resource.TIN, ResourceType.DUST);
+    public static final RegistryObject<Item> URANIUM_DUST   =
+            registerResource(Resource.URANIUM, ResourceType.DUST);
 
     //enhanced
+    public static final RegistryObject<Item> ALUMINIUM_ENHANCED =
+            registerResource(Resource.ALUMINIUM, ResourceType.ENHANCED, true);
+    public static final RegistryObject<Item> DIAMOND_ENHANCED   =
+            registerResource(Resource.DIAMOND, ResourceType.ENHANCED, true);
+    public static final RegistryObject<Item> IRON_ENHANCED      =
+            registerResource(Resource.IRON, ResourceType.ENHANCED, true);
+    public static final RegistryObject<Item> PLATINUM_ENHANCED  =
+            registerResource(Resource.PLATINUM, ResourceType.ENHANCED, true);
+    public static final RegistryObject<Item> RUBY_ENHANCED      =
+            registerResource(Resource.RUBY, ResourceType.ENHANCED, true);
+    public static final RegistryObject<Item> SAPPHIRE_ENHANCED  =
+            registerResource(Resource.SAPPHIRE, ResourceType.ENHANCED, true);
+    public static final RegistryObject<Item> STEEL_ENHANCED     =
+            registerResource(Resource.STEEL, ResourceType.ENHANCED, true);
 
     //ingots
+    public static final RegistryObject<Item> ALUMINIUM_INGOT =
+            registerResource(Resource.ALUMINIUM, ResourceType.INGOT);
+    public static final RegistryObject<Item> COPPER_INGOT    =
+            registerResource(Resource.COPPER, ResourceType.INGOT);
+    public static final RegistryObject<Item> PLATINUM_INGOT  =
+            registerResource(Resource.PLATINUM, ResourceType.INGOT);
+    public static final RegistryObject<Item> RUBY_INGOT      =
+            registerResource(Resource.RUBY, ResourceType.INGOT);
+    public static final RegistryObject<Item> SAPPHIRE_INGOT  =
+            registerResource(Resource.SAPPHIRE, ResourceType.INGOT);
+    public static final RegistryObject<Item> STEEL_INGOT     =
+            registerResource(Resource.STEEL, ResourceType.INGOT);
+    public static final RegistryObject<Item> TIN_INGOT       =
+            registerResource(Resource.TIN, ResourceType.INGOT);
+    public static final RegistryObject<Item> URANIUM_INGOT   =
+            registerResource(Resource.URANIUM, ResourceType.INGOT);
 
     //purified
+    public static final RegistryObject<Item> PLATINUM_PURIFIED =
+            registerResource(Resource.PLATINUM, ResourceType.PURIFIED);
+    public static final RegistryObject<Item> STEEL_PURIFIED    =
+            registerResource(Resource.STEEL, ResourceType.PURIFIED);
+    public static final RegistryObject<Item> URANIUM_PURIFIED  =
+            registerResource(Resource.URANIUM, ResourceType.PURIFIED);
 
 
     public static RegistryObject<Item> registerResource(Resource resource,
                                                         ResourceType type) {
         CraftleItem item = new CraftleItem(resource, type);
+        return ITEMS.register(item.getCraftleRegistryName(), () -> item);
+    }
+
+    public static RegistryObject<Item> registerResource(Resource resource,
+                                                        ResourceType type,
+                                                        boolean hasEffect) {
+        CraftleItem item = new CraftleItem(resource, type, hasEffect);
         return ITEMS.register(item.getCraftleRegistryName(), () -> item);
     }
 
