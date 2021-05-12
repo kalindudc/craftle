abf23a3b22ced0bb1efe16fb0c9d69b662df45df
Kalindu De Costa <kalinduk.decosta@gmail.com>
2021-05-12 19:56:58 -0400

    Bring over changes from develop
    Signed-off-by: Kalindu De Costa <kalinduk.decosta@gmail.com>

--- src/main/java/com/craftle_mod/api/constants/FluidConstants.java
+++ src/main/java/com/craftle_mod/api/constants/FluidConstants.java
@@ -1,19 +1,6 @@
 package com.craftle_mod.api.constants;
 
-import com.craftle_mod.api.ColorData;
-import com.craftle_mod.common.registries.CraftleBlocks;
-import com.craftle_mod.common.registries.CraftleFluids;
-import net.minecraft.block.FlowingFluidBlock;
-import net.minecraftforge.fluids.FluidAttributes;
-import net.minecraftforge.fluids.ForgeFlowingFluid;
-import net.minecraftforge.fluids.ForgeFlowingFluid.Properties;
-
 public class FluidConstants {
 
-    public static final ForgeFlowingFluid.Properties ENERGY_FLUID_PROPERTIES = new Properties(
-        CraftleFluids.ENERGY_FLUID, CraftleFluids.ENERGY_FLOWING,
-        FluidAttributes.builder(BlockConstants.ENERGY_STILL, BlockConstants.ENERGY_FLOWING)
-            .color(ColorData.ENERGY.getColorCode()).overlay(BlockConstants.ENERGY_FLUID_OVERLAY))
-        .block(() -> (FlowingFluidBlock) CraftleBlocks.ENERGY.get());
 
 }
