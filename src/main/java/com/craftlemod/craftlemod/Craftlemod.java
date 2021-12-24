f3f427513df927c03ba57d5b81bd783a79e5738d
Kalindu De Costa <kalinduk.decosta@gmail.com>
2021-12-24 14:42:57 -0500

    Initial fabric files

---
+++ src/main/java/com/craftlemod/craftlemod/Craftlemod.java
@@ -0,0 +1,11 @@
+package com.craftlemod.craftlemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Craftlemod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/craftlemod/craftlemod/client/CraftlemodClient.java
@@ -0,0 +1,14 @@
+package com.craftlemod.craftlemod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class CraftlemodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
